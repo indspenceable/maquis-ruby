edc2a124ba3b24c4aa96120c641b0c60fb2ad6a2
Daniel Spencer <dspencer@squareup.com>
2014-02-14 19:57:56 -0800

    remove unused level generation code

--- app/level_generator.rb
+++ app/level_generator.rb
@@ -165,30 +165,6 @@ module LevelGenerator
       end
     end
 
-    def place_units_in_far_away_circles(level, baddie_units, player_units)
-      # now, find some points to center them on. We'll start with just one point for now.
-      begin
-        px, py = rand(level.map_size_x), rand(level.map_size_y)
-        bx, by = rand(level.map_size_x), rand(level.map_size_y)
-        player_area = Path.discover_paths(FakeUnit.new(px,py), level, 4)
-        baddie_area = Path.discover_paths(FakeUnit.new(bx,by), level, 4)
-        path_between = Path.find(FakeUnit.new(px,py), bx, by, level, 70, :ignore)
-      end while false ||
-        map(px, py) == :wall || map(bx, by) == :wall
-        player_area.count < player_units.size ||
-        baddie_area.count < baddie_units.size ||
-        !path_between || path_between.length < min_distance
-      player_area.map!(&:last_point)
-      baddie_area.map!(&:last_point)
-      player_area.shuffle!
-      baddie_area.shuffle!
-      return false unless place_units_in_area(player_units, player_area, level)
-      return false unless place_units_in_area(baddie_units, baddie_area, level)
-      level.units.concat(baddie_units.each{|u| u.current_level = level})
-
-      return true
-    end
-
     def place_units_with_baddies_scattered(level, baddie_units, player_units)
       baddie_boss = baddie_units.shift
       whole_map = level.map_size_x.times.to_a.product(level.map_size_y.times.to_a)
@@ -380,60 +356,6 @@ module LevelGenerator
 
     def terrain_fortune
       "Your next fight will take place in a mountain pass. Mountaineers might let you avoid some chokepoints..."
-    end
-
-    def goal
-      :kill_enemies
-    end
-
-    def fog_of_war
-      false
-    end
-
-    def min_distance
-      10
-    end
-  end
-
-  class Forest < Base
-    def min_distance
-      10
-    end
-
-    def generate_map
-      # might not generate a good map the first time, so loop until we do.
-      while true
-        # start with an empy level, fill it with tiles randomly
-        l = Level.new(MAP_SIZE_X, MAP_SIZE_Y)
-        starting_mountains = 15.times.map{[rand(MAP_SIZE_X), rand(MAP_SIZE_Y)]}
-        l.fill {|x,y| starting_mountains.include?([x,y]) ? :forest : :plains }
-
-        # use cellular attomata to iterate 5 times over this map
-        other_map = Array.new(MAP_SIZE_X){ Array.new(MAP_SIZE_Y) }
-        3.times do
-          (MAP_SIZE_X).times do |x|
-            (MAP_SIZE_Y).times do |y|
-              next other_map[x][y] = :mountain if border?(x,y,l)
-              count = 0
-              3.times do |_i|
-                i = _i-1
-                3.times do |_j|
-                  j = _j-1
-                  count += 1 if l.map(x+i,y+j) == :forest
-                end
-              end
-              other_map[x][y] = (count >= 1) && (rand(100) < 25) ? :forest :  (rand(100) < 10) ? :fort : :plains
-            end
-          end
-          l.fill { |x,y| other_map[x][y] }
-        end
-
-        return l
-      end
-    end
-
-    def terrain_fortune
-      "Your next fight will take place in a dense forest. Calvalry will have a hard time omving around."
     end
 
     def goal
