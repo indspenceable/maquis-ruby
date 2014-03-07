c398e72f997651d2d326bc592e240968e6f17f1b
Daniel Spencer <dspencer@squareup.com>
2014-03-06 17:27:39 -0800

    TODOs

--- app/items/staff.rb
+++ app/items/staff.rb
@@ -1,2 +1,43 @@
 class Staff
 end
+
+class HealingStaff < Staff
+  def pretty
+    "healing staff"
+  end
+  def activate!(me, target, level)
+    puts "Activated the healing staff."
+  end
+  def used_up?
+    false
+  end
+  def valid_target?(t)
+    true
+  end
+end
+
+# Planned Staves / Wands
+=begin
+  Warp
+  Rescue
+  Rewarp (self tele)
+  Retreat (remove a unit from battle for this level)
+  Recover (heal status)
+  Heal
+    standard
+    + value
+    + all valid targets
+    ^ all the above, at range, as well.
+  Torch
+  Unlock
+  Status Buffs
+---------
+  Repel (enemy unit)
+  Summon (Enemy unit)
+  Stasis/Rift (enemy unit can't move or do anything for a few turns)
+    - Stasis leaves a blocker in their space, rift does not)
+  Inflict (gives them a status effect, like poison, etc)
+  Shade (enemy only?)
+  Lock (perminantly blocks off a door.)
+  Status debuffs (str, magic, mag def, def, speed)
+=end
