267b5566b5aed36d156a5398fd07125584e86147
Daniel Spencer <dspencer@squareup.com>
2014-03-07 00:12:31 -0800

    actually add wands.

--- app/items/staff.rb
+++ app/items/staff.rb
@@ -6,7 +6,7 @@ class HealingStaff < Staff
     "healing staff"
   end
   def activate!(me, target, level)
-    puts "Activated the healing staff."
+    target.heal(me.power + 10)
   end
   def used_up?
     false
---
+++ app/items/wands.rb
@@ -0,0 +1,17 @@
+class Wand
+end
+
+class PoisonWand < Wand
+  def pretty
+    "poison wand"
+  end
+  def activate!(me, target, level)
+    target.buff!('poison', 2, 5)
+  end
+  def used_up?
+    false
+  end
+  def valid_target?(t)
+    true
+  end
+end
